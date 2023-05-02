bb67c59f54c2e156fb0588c2acd52cb4afa7a8b3
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-05-02 21:59:36 +0200

    added test data files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,124 @@
 from pathlib import Path
 
+import pandas as pd
 import pytest
 
+from readnext.utils import load_df_from_pickle, load_object_from_pickle
 
-@pytest.fixture
+
+@pytest.fixture(scope="session")
 def root_path() -> Path:
     """Return project root path when pytest is executed from the project root directory."""
     return Path().cwd()
+
+
+@pytest.fixture(scope="session")
+def test_bert_cosine_similarities_most_cited(root_path: Path) -> pd.DataFrame:
+    return load_df_from_pickle(
+        root_path / "tests" / "data" / "test_bert_cosine_similarities_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_bert_embeddings_mapping_most_cited(root_path: Path) -> dict:
+    return load_object_from_pickle(
+        root_path / "tests" / "data" / "test_bert_embeddings_mapping_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_bert_tokenized_abstracts_mapping_most_cited(root_path: Path) -> dict:
+    return load_object_from_pickle(
+        root_path / "tests" / "data" / "test_bert_tokenized_abstracts_mapping_most_cited.pt"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_bibliographic_coupling_scores_most_cited(root_path: Path) -> pd.DataFrame:
+    return load_df_from_pickle(
+        root_path / "tests" / "data" / "test_bibliographic_coupling_scores_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_co_citation_analysis_scores_most_cited(root_path: Path) -> pd.DataFrame:
+    return load_df_from_pickle(
+        root_path / "tests" / "data" / "test_co_citation_analysis_scores_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_documents_authors_labels_citations_most_cited(root_path: Path) -> pd.DataFrame:
+    return load_df_from_pickle(
+        root_path / "tests" / "data" / "test_documents_authors_labels_citations_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_fasttext_cosine_similarities_most_cited(root_path: Path) -> pd.DataFrame:
+    return load_df_from_pickle(
+        root_path / "tests" / "data" / "test_fasttext_cosine_similarities_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_fasttext_embeddings_mapping_most_cited(root_path: Path) -> dict:
+    return load_object_from_pickle(
+        root_path / "tests" / "data" / "test_fasttext_embeddings_mapping_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_scibert_cosine_similarities_most_cited(root_path: Path) -> pd.DataFrame:
+    return load_df_from_pickle(
+        root_path / "tests" / "data" / "test_scibert_cosine_similarities_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_scibert_embeddings_mapping_most_cited(root_path: Path) -> dict:
+    return load_object_from_pickle(
+        root_path / "tests" / "data" / "test_scibert_embeddings_mapping_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_scibert_tokenized_abstracts_mapping_most_cited(root_path: Path) -> dict:
+    return load_object_from_pickle(
+        root_path / "tests" / "data" / "test_scibert_tokenized_abstracts_mapping_most_cited.pt"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_spacy_tokenized_abstracts_most_cited(root_path: Path) -> pd.DataFrame:
+    return load_df_from_pickle(
+        root_path / "tests" / "data" / "test_spacy_tokenized_abstracts_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_tfidf_cosine_similarities_most_cited(root_path: Path) -> pd.DataFrame:
+    return load_df_from_pickle(
+        root_path / "tests" / "data" / "test_tfidf_cosine_similarities_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_tfidf_embeddings_mapping_most_cited(root_path: Path) -> dict:
+    return load_object_from_pickle(
+        root_path / "tests" / "data" / "test_tfidf_embeddings_mapping_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_word2vec_cosine_similarities_most_cited(root_path: Path) -> pd.DataFrame:
+    return load_df_from_pickle(
+        root_path / "tests" / "data" / "test_word2vec_cosine_similarities_most_cited.pkl"
+    )
+
+
+@pytest.fixture(scope="session")
+def test_word2vec_embeddings_mapping_most_cited(root_path: Path) -> dict:
+    return load_object_from_pickle(
+        root_path / "tests" / "data" / "test_word2vec_embeddings_mapping_most_cited.pkl"
+    )

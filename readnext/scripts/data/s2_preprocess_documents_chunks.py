83959c1e25d34cde2e73bedcfcb4c784d4df1856
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-05-11 13:12:25 +0200

    added unit tests for cosine similarity class (#75)

--- readnext/scripts/data/s2_preprocess_documents_chunks.py
+++ readnext/scripts/data/s2_preprocess_documents_chunks.py
@@ -9,8 +9,7 @@ from pathlib import Path
 import pandas as pd
 
 from readnext.config import DataPaths
-from readnext.utils import setup_progress_bar
-from readnext.utils.preprocessing import add_rank
+from readnext.utils import add_rank, setup_progress_bar
 
 
 def flatten_list_of_dicts(list_of_dicts: list[dict], key: str) -> list[str]:

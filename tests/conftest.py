b40fcdc4e1767385bbdeb0ff2e3f01d1764730b8
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-05-25 21:01:00 +0200

    fixed all tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-def filepath_to_modules(filepath: Path) -> str:
+def filepath_to_module_path(filepath: Path) -> str:
     return str(filepath).replace("/", ".").replace(".py", "")
 
 
@@ -10,15 +10,16 @@ def list_fixture_modules() -> list[str]:
     List the relative paths (`tests/fixtures/*.py`) of all fixture files and display the
     filepaths as module paths.
 
-    Example: - relative filepath: `tests/fixtures/test_data.py` - module path:
-    `tests.fixtures.test_data`
+    Example:
+    - relative filepath: `tests/fixtures/test_data.py`
+    - module path: `tests.fixtures.test_data`
     """
     root_path = Path(__file__).parent.parent
     tests_dirpath = root_path / "tests"
     fixtures_dirpath = tests_dirpath / "fixtures"
 
     return [
-        filepath_to_modules(fixture_filepath.relative_to(root_path))
+        filepath_to_module_path(fixture_filepath.relative_to(root_path))
         for fixture_filepath in fixtures_dirpath.glob("*.py")
         # exclude `__init__.py` files
         if "__" not in str(fixture_filepath)

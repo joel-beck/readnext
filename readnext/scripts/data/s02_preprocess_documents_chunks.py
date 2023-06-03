aae8fbdf659bd4cc56690d044d65cbd687c687af
Joel Beck <81745480+joel-beck@users.noreply.github.com>
2023-06-03 17:52:05 +0200

    continued scripts refactoring

--- readnext/scripts/data/s02_preprocess_documents_chunks.py
+++ readnext/scripts/data/s02_preprocess_documents_chunks.py
@@ -16,39 +16,18 @@ def flatten_list_of_dicts(list_of_dicts: list[dict], key: str) -> list[str]:
     return [d[key] for d in list_of_dicts] if list_of_dicts is not None else []
 
 
-def remove_duplicates(list_: list[str]) -> list[str]:
-    return list(set(list_))
-
-
-def extract_arxiv_id(df: pl.DataFrame) -> pl.Series:
-    # keep only dictionaries where arxiv id is not None
-    return df["externalids"].apply(lambda d: d["ArXiv"])
-
-
-def filter_by_tag(df: pl.DataFrame, tag: str) -> pl.DataFrame:
-    return df.filter(pl.col("tags").apply(lambda x: tag in x))
-
-
 def preprocess_document_chunk(filepath: Path, chunk_index: int) -> None:
     documents_long_format = (
         documents_ranked.with_columns(
             author_id=pl.col("authors").apply(lambda x: flatten_list_of_dicts(x, "authorId")),
             author_name=pl.col("authors").apply(lambda x: flatten_list_of_dicts(x, "name")),
-            tags=pl.col("s2fieldsofstudy")
-            .apply(lambda x: flatten_list_of_dicts(x, "category"))
-            .apply(remove_duplicates),
         )
         .explode(["author_id", "author_name"])
         .drop_nulls(subset=["author_id", "author_name"])
         .with_columns(
             author_id=pl.col("author_id").cast(pl.Int64),
         )
-        .pipe(filter_by_tag, tag="Computer Science")
     )
-
-    documents_long_format_arxiv = documents_long_format.with_columns(
-        arxiv_id=extract_arxiv_id(documents_long_format)
-    ).filter(pl.col("arxiv_id").is_not_null())
 
 
 def main() -> None:
@@ -68,31 +47,41 @@ def main() -> None:
         "arxiv_labels",
     ]
 
-    documents = pl.scan_parquet(DataPaths.d3.documents_parquet, n_rows=1000).collect()
-    arxiv_labels = pl.scan_parquet(DataPaths.d3.arxiv_labels_parquet, n_rows=1000).collect()
-
-    documents = documents.rename(
-        {
-            "corpusid": "d3_document_id",
-            "publicationdate": "publication_date",
-            "year": "publication_year",
-            "citationcount": "citationcount_document",
-            "url": "semanticscholar_url",
-            "s2fieldsofstudy": "semanticscholar_tags",
-        }
-    )
+    documents = pl.scan_parquet(DataPaths.d3.documents_parquet, n_rows=10000)
+    arxiv_labels = pl.scan_parquet(DataPaths.d3.arxiv_labels_parquet, n_rows=10000)
 
     arxiv_labels = (
         arxiv_labels.rename({"id": "arxiv_id", "categories": "arxiv_labels"})
-        .select(["arxiv_id", "arxiv_labels"])
-        .with_columns(arxiv_url=pl.col("arxiv_id").apply(get_arxiv_url_from_arxiv_id))
+        .filter(pl.col("arxiv_labels").str.contains("cs.", literal=True))
+        .with_columns(
+            arxiv_url=pl.col("arxiv_id").apply(get_arxiv_url_from_arxiv_id),
+            arxiv_labels=pl.col("arxiv_labels").str.split(" "),
+        )
+        .select(["arxiv_id", "arxiv_url", "arxiv_labels"])
+    )
+
+    documents_labels = (
+        documents.rename(
+            {
+                "corpusid": "d3_document_id",
+                "publicationdate": "publication_date",
+                "year": "publication_year",
+                "citationcount": "citationcount_document",
+                "url": "semanticscholar_url",
+                "s2fieldsofstudy": "semanticscholar_tags",
+            }
+        )
+        .with_columns(arxiv_id=pl.col("externalids").apply(lambda d: d["ArXiv"], skip_nulls=False))
+        .drop_nulls(subset=["arxiv_id"])
+        .join(arxiv_labels, on="arxiv_id", how="inner")
+        .collect()
     )
 
     # TODO:
-    # 1. Extract author id, author name and arxiv id from dictionary
-    # 2. Convert to long format with one row per author
-    # 3. Merge Arxiv labels based on Arxiv id
-    # 4. Filter to keep only documents with Arxiv id within computer science
+    # 1. Fill missing publication date values with publication year (see script 10)
+    # 2. Extract author id, and author name from dictionary
+    # 3. Convert to long format with one row per author
+    # 4. Make code modular by using functions that return expression
     # 5. Write merged dataframe to parquet
 
 
